87349ac1242e92d6cc21a07bd17a39b15a3813eb
Yago <yarlorenzo@gmail.com>
2023-11-01 22:21:47 +0100

    Removed typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 module.exports = {
-  entry: './src/index.tsx',
+  entry: './src/index.js',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
